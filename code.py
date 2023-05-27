6849b23bf1d33f3fb8534132bcb379fb8b2446c3
BradWithGithub <91183171+awrdrfs@users.noreply.github.com>
2023-05-27 10:41:27 +0800

    Update code.py

--- code.py
+++ code.py
@@ -18,3 +18,12 @@ cd /content/yolov7
 python detect.py --img-size 640 --conf 0.25 --source '/content/testhuman.jpg'\
  --weight '/content/gdrive/MyDrive/Colab Notebooks/faceDetect10/weights/best.pt' --save-txt --device 0\
  --name faceDetect_test --project '/content/gdrive/MyDrive/Colab Notebooks'
+
+#繼續訓練
+%%shell
+cd yolov7
+
+python train.py --workers 1 --batch-size 8 --epochs 100 --img 640 640\
+  --data '/content/dataFaceDetect/mydata.yaml' --weights '/content/gdrive/MyDrive/Colab Notebooks/faceDetect10/weights/best.pt' --device 0 --cfg 'cfg/training/yolov7.yaml'\
+  --name faceDetect --hyp 'data/hyp.scratch.p5.yaml' --project '/content/gdrive/MyDrive/Colab Notebooks'\
+  --resume '/content/gdrive/MyDrive/Colab Notebooks/faceDetect11/weights/last.pt'
